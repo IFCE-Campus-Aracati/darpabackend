036a2ed372707d8557783ebb60fd888a436f23f0
Éolo Charles <eolocharles@gmail.com>
2023-05-06 10:13:09 -0300

    add gitignore

---
+++ printer-service/src/main/java/br/com/ifce/darpa/printerservice/PrinterServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifce.darpa.printerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrinterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrinterServiceApplication.class, args);
+	}
+
+}
